28759bf10995e5114619fe33b0ef8c34484cb9b1
cuppi <cuppi413@163.com>
2017-05-26 19:55:58 +0800

    更新

--- publish.js
+++ publish.js
@@ -19,12 +19,29 @@ exec('npm run generate-docs', {cwd: comment}, (error, stdout, stderr) => {
 
 
 function commitDoc() {
-  exec('git commit', (error, stdout, stderr) => {
-    updateDoc();
+  exec('git add comment/*', (error, stdout, stderr) => {
+    exec('git commit comment/* -m "commit doc comment"', (error, stdout, stderr) => {
+      if (error) {
+        console.log('提交失败');
+        console.error(`exec error: ${error}`);
+        updateDoc();
+        return;
+      }
+      exec('git push origin master', (error, stdout, stderr) => {
+        if (error) {
+          console.log('推送远程库失败');
+          console.error(`exec error: ${error}`);
+        } else {
+          console.log('推送远程库成功...');
+        }
+        updateDoc();
+      });
+    });
   });
 }
 
 function updateDoc() {
+  console.log('开始更新文档...');
   conn.on('ready', function () {
     // console.log('Client :: ready');
     conn.exec('cd /opt/project/NodeWorld/jbzweb-sdk && node ./publish.js', function (err, stream) {
@@ -32,7 +49,7 @@ function updateDoc() {
       stream.on('close', function (code, signal) {
         console.log('更新文档成功');
         // console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-        // conn.end();
+        conn.end();
       }).on('data', function (data) {
       }).stderr.on('data', function (data) {
         console.log('STDERR: ' + data);
