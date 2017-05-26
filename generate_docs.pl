fb707357ef488d072165cedf159ff273b7b75c97
cuppi <cuppi413@163.com>
2017-05-26 11:58:32 +0800

    更新

--- generate_docs.pl
+++ generate_docs.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
-system('rm -rf out');
+system('rm -rf comment');
 #system('jsdoc src/* -c jsdoc.config.json -t ./node_modules/ink-docstrap/template -R README.md -r .');
 #system('jsdoc src/* -r -c jsdoc.config.json');
 
-system('jsdoc -c jsdoc.config.json -t ./node_modules/ink-docstrap/template -R README.md -r .');
+system('jsdoc -c jsdoc.config.json --destination ./comment -t ./node_modules/ink-docstrap/template -R README.md -r .');
