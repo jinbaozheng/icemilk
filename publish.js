b5dcbbcbdf12e883a0831920dfc8ec97d546dc21
cuppi <cuppi413@163.com>
2017-05-26 19:06:15 +0800

    更新

--- publish.js
+++ publish.js
@@ -5,21 +5,34 @@
 var Client = require('/usr/local/lib/node_modules/ssh2').Client;
 var conn = new Client();
 
-conn.on('ready', function () {
-    console.log('Client :: ready');
+const exec = require('child_process').exec;
+
+exec('npm run generate-docs', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  update();
+});
+
+function update() {
+  conn.on('ready', function () {
+    // console.log('Client :: ready');
     conn.exec('cd /opt/project/NodeWorld/jbzweb-sdk && node ./publish.js', function (err, stream) {
-        if (err) throw err;
-        stream.on('close', function (code, signal) {
-            // console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-            // conn.end();
-        }).on('data', function (data) {
-            console.log('更新文档成功');
-        }).stderr.on('data', function (data) {
-            console.log('STDERR: ' + data);
-        });
+      if (err) throw err;
+      stream.on('close', function (code, signal) {
+        console.log('更新文档成功');
+        // console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+        // conn.end();
+      }).on('data', function (data) {
+      }).stderr.on('data', function (data) {
+        console.log('STDERR: ' + data);
+      });
     });
-}).connect({
+  }).connect({
     host: '10.1.1.61',
     username: 'root',
     password: 'JBZ321*88'
-});
+  });
+}
+
