f95f1238b7a9301c5ed9298ac39c8619cd13e90f
cuppi <cuppi413@163.com>
2018-08-28 17:04:57 +0800

    去除测试文件

--- test.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let fileName = '/Users/cuppi/Desktop/IDouPiao/sdktester/src/jbzwebsdk/node_modules/ink-docstrap/template/static/fonts/glyphicons-halflings-regular.eot';
-let toFile = './comment/fonts/glyphicons-halflings-regular.eot';
-fs.copyFileSync( fileName, toFile );
