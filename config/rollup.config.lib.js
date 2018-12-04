75af922cc7c1bea5b02e569c87ed54c0a7251846
cuppi <cuppi413@163.com>
2018-12-04 16:36:52 +0800

    增加lib打包 更改package.json 指向到lib

---
+++ config/rollup.config.lib.js
@@ -0,0 +1,21 @@
+import typescript from 'rollup-plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import { uglify } from "rollup-plugin-uglify";
+module.exports = () => {
+    return {
+        input: './src/index.ts',
+        output: {
+            file: './lib/index.js',
+            format: 'cjs'
+        },
+        plugins: [
+            json(),
+            resolve(),
+            commonjs(),
+            typescript({lib: ["es5", "es6", "dom"], target: "es5"}),
+            uglify()
+        ]
+    }
+};
