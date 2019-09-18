25728848bd1cdd430c8f07e7d28c155dcea99576
cuppi <cuppi413@163.com>
2019-09-18 16:37:31 +0800

    增加 rollup-plugin-sourcemaps 依赖包及增加rollup link配置文件

---
+++ config/rollup.config.link.js
@@ -0,0 +1,29 @@
+import typescript from 'rollup-plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+module.exports = () => {
+    return {
+        input: './src/index.ts',
+        output: {
+            sourceMap: true,
+            name: 'icemilk',
+            globals: {
+                moment: 'moment',
+                axios: 'axios'
+            },
+            file: './lib/index.js',
+            format: 'cjs'
+        },
+        plugins: [
+            json(),
+            resolve(),
+            commonjs(),
+            typescript({lib: ["es5", "es6", "dom"], target: "es5"}),
+            sourcemaps()
+        ],
+        external: [ 'moment', 'axios' ],
+        treeshake: false
+    }
+};
