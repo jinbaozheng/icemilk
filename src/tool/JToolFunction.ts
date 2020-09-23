eaa0efbd29e9d3a8830986ea2fb9384d4a4fc857
cuppi <cuppi413@163.com>
2020-09-23 16:42:38 +0800

    修复 curry方法 参数调用完整后继续调用出现的实参混乱问题

--- src/tool/JToolFunction.ts
+++ src/tool/JToolFunction.ts
@@ -1,18 +1,15 @@
 class JToolFunction{
     static curry(func: Function, arity?: number): Function{
-
         function wrap(func: Function, arity?: number, cacheArgs: any[] = []) {
             arity = arity ?? func.length;
             return function(...args) {
-                cacheArgs = [...cacheArgs, ...args];
-                if (cacheArgs.length < arity){
-                    return wrap(func, arity, cacheArgs);
+                if (cacheArgs.length + args.length < arity){
+                    return wrap(func, arity, [...cacheArgs, ...args]);
                 } else {
-                    return func.call(this, ...cacheArgs)
+                    return func.call(this, ...cacheArgs, ...args)
                 }
             }
         }
-
         return wrap(func, arity);
     }
 }
